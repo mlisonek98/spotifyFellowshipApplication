5bd84cb86097a45ae866e34552ab2011220040f4
Marko Lisonek <mlisonek98@gmail.com>
2018-06-08 16:15:29 -0400

    question 3 completed

--- applicationChallenge.js
+++ applicationChallenge.js
@@ -1,7 +1,5 @@
 //QUESTION 1
 
-//For s = "weather" and t = "therapyw" sortByString(s, t) = "theeraw"
-
 const sortByString = function(s, t) {
   let result = "";
   let hash = {};
@@ -25,18 +23,41 @@ const sortByString = function(s, t) {
 };
 
 //QUESTION 2
+//Pretty hard but I think I got it
 
 const decodeString = function(str) {
-  let splitStr = str.split('')
-  let currentSubString = ''
-  for(let i = splitStr.length - 1; i > 0; i--) {
-    let isNumber = parseInt(splitStr[i])
-    if(splitStr[i] !== ']' && splitStr[i] !== '[' && !isNumber){
-      currentSubString += splitStr[i]
+  let splitStr = str.split("");
+  let currentSubString = "";
+  for (let i = splitStr.length - 1; i > 0; i--) {
+    let isNumber = parseInt(splitStr[i]);
+    if (splitStr[i] !== "]" && splitStr[i] !== "[" && !isNumber) {
+      currentSubString += splitStr[i];
     }
-    if(splitStr[i] === '[') {
-      currentSubString = currentSubString.split("").reverse().join("").repeat(parseInt(splitStr[i-1]))
+    if (splitStr[i] === "[") {
+      currentSubString = currentSubString
+        .split("")
+        .reverse()
+        .join("")
+        .repeat(parseInt(splitStr[i - 1]));
     }
   }
-  return currentSubString
+  return currentSubString;
+};
+
+//QUESTION 3
+
+function changePossibilities(N, coins) {
+  const possibleWays = [];
+  for (let i = 1; i <= N; i++) {
+    possibleWays[i] = 0;
+  }
+  possibleWays[0] = 1;
+  coins.forEach(function(coin) {
+    for (let j = coin; j <= N; j++) {
+      let remainder = j - coin;
+      possibleWays[j] += possibleWays[remainder];
+    }
+  });
+
+  return possibleWays[N];
 }
