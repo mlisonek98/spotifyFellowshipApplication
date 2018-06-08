5d3a66e4c65653f1ab2304c36cd297b02e0d22c8
Marko Lisonek <mlisonek98@gmail.com>
2018-06-08 16:04:31 -0400

    second question complete

--- applicationChallenge.js
+++ applicationChallenge.js
@@ -2,25 +2,41 @@
 
 //For s = "weather" and t = "therapyw" sortByString(s, t) = "theeraw"
 
-
-const sortByString = function (s, t) {
-  let result = ''
-  let hash = {}
-  for(let j = 0; j < s.length; j++) {
-    if(hash[s[j]]) {
-      hash[s[j]][1] += 1
+const sortByString = function(s, t) {
+  let result = "";
+  let hash = {};
+  for (let j = 0; j < s.length; j++) {
+    if (hash[s[j]]) {
+      hash[s[j]][1] += 1;
     } else {
-      hash[s[j]] = [s[j], 1]
+      hash[s[j]] = [s[j], 1];
     }
   }
-  let i = 0
-  let y = 0
-  while(i < t.length){
-    if(hash[t[i]]) {
-      result += hash[t[i]][0].repeat(hash[t[i]][1])
-      y++
+  let i = 0;
+  let y = 0;
+  while (i < t.length) {
+    if (hash[t[i]]) {
+      result += hash[t[i]][0].repeat(hash[t[i]][1]);
+      y++;
     }
-    i++
+    i++;
   }
-  return result
+  return result;
+};
+
+//QUESTION 2
+
+const decodeString = function(str) {
+  let splitStr = str.split('')
+  let currentSubString = ''
+  for(let i = splitStr.length - 1; i > 0; i--) {
+    let isNumber = parseInt(splitStr[i])
+    if(splitStr[i] !== ']' && splitStr[i] !== '[' && !isNumber){
+      currentSubString += splitStr[i]
+    }
+    if(splitStr[i] === '[') {
+      currentSubString = currentSubString.split("").reverse().join("").repeat(parseInt(splitStr[i-1]))
+    }
+  }
+  return currentSubString
 }
